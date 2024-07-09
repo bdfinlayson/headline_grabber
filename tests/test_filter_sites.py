335249f93d7be3df261f60d7b4d15609ce173b96
Sheena309 <sheena.munjal6@gmail.com>
2024-07-09 13:12:37 -0500

    removed print statements and commented code lines

--- tests/test_filter_sites.py
+++ tests/test_filter_sites.py
@@ -7,8 +7,6 @@ from src.headline_grabber.pipeline_steps.filter_sites import FilterSites
 from src.headline_grabber.configurations.sites import sites
 from src.headline_grabber.models.news_site import NewsSite
 
-#comamnd to run this test file : pytest -s .\test_filter_sites.py
-
 @pytest.fixture
 def sample_sites():
     return copy.copy(sites)
@@ -31,8 +29,7 @@ def test_include_option(sample_sites):
     result_abbreviations = {site.abbreviation for site in result_context.site_configs}
     assert len(result_context.site_configs) == len(expected_abbreviations)
     assert result_abbreviations == expected_abbreviations
-    print("***test_include_option passed***")
-
+    
 def test_exclude_option(sample_sites):
     total_sites = len(sample_sites)
     user_input = UserPreferences(exclude=["nyt"])
@@ -48,8 +45,7 @@ def test_exclude_option(sample_sites):
     expected_length = total_sites - 1
     assert len(result_context.site_configs) == expected_length
     assert all(site.abbreviation != "nyt" for site in result_context.site_configs)
-    print("***test_exclude_option passed***")
-
+   
 
 def test_include_and_exclude_option(sample_sites):
     user_input = UserPreferences(include=["nyt"], exclude=["bbc"])
@@ -61,8 +57,6 @@ def test_include_and_exclude_option(sample_sites):
         documents_for_display={}
     )
     filter_sites = FilterSites()
-    #result_context = filter_sites.run(context)
     with pytest.raises(click.BadParameter):
         filter_sites.run(context)
-    print("***test_include_and_exclude_option passed***")
-
+   
