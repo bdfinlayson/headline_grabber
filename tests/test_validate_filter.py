ba70aacae15ffd49954e21499bfac144d3c12e17
mralderman <aldermanza@gmail.com>
2024-07-23 18:36:28 -0400

    Added new unit tests to check positive and negative

--- tests/test_validate_filter.py
+++ tests/test_validate_filter.py
@@ -5,13 +5,17 @@ from headline_grabber.validators.click.option_validator import OptionValidator
 def test_validate_filter_sentiment():
     # Test cases for valid input
     assert OptionValidator.validate_filter_sentiment(None, None, 'POS') == 'POSITIVE'
+    assert OptionValidator.validate_filter_sentiment(None, None, 'PoS') == 'POSITIVE'
     assert OptionValidator.validate_filter_sentiment(None, None, 'pos') == 'POSITIVE'
     assert OptionValidator.validate_filter_sentiment(None, None, 'POSITIVE') == 'POSITIVE'
     assert OptionValidator.validate_filter_sentiment(None, None, 'positive') == 'POSITIVE'
+    assert OptionValidator.validate_filter_sentiment(None, None, 'PoSiTiVe') == 'POSITIVE'
     assert OptionValidator.validate_filter_sentiment(None, None, 'NEG') == 'NEGATIVE'
+    assert OptionValidator.validate_filter_sentiment(None, None, 'NeG') == 'NEGATIVE'
     assert OptionValidator.validate_filter_sentiment(None, None, 'neg') == 'NEGATIVE'
     assert OptionValidator.validate_filter_sentiment(None, None, 'NEGATIVE') == 'NEGATIVE'
     assert OptionValidator.validate_filter_sentiment(None, None, 'negative') == 'NEGATIVE'
+    assert OptionValidator.validate_filter_sentiment(None, None, 'NeGaTiVe') == 'NEGATIVE'
 
     # Test cases for invalid input
     with pytest.raises(click.BadParameter):
@@ -22,6 +26,12 @@ def test_validate_filter_sentiment():
 
     with pytest.raises(click.BadParameter):
         OptionValidator.validate_filter_sentiment(None, None, 'positive123')
+
+    with pytest.raises(click.BadParameter):
+        OptionValidator.validate_filter_sentiment(None, None, 'positeve')
+
+    with pytest.raises(click.BadParameter):
+        OptionValidator.validate_filter_sentiment(None, None, 'negitive')
 
     # Test cases for None or empty input
     assert OptionValidator.validate_filter_sentiment(None, None, None) is None
